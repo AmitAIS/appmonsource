b8db51f1c0880991c3b0b999b18d5fe2fe8b0711
skuchi <sk.kuchi.hcu@gmail.com>
2015-01-01 04:18:36 +0530

    adding controller files and commented pagination in Build

---
+++ Appmon.Dashboard/Controllers/AppSummaryController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Appmon.Dashboard.Models;
+using Newtonsoft.Json;
+
+namespace Appmon.Dashboard.Controllers
+{
+    public class AppSummaryController : ApiController
+    {
+        public ApplicationSummary Get()
+        {
+            return (ApplicationSummary)JsonConvert.DeserializeObject(DocumentService.GetResult("ApplicationSummarycollection"), typeof(ApplicationSummary));
+        }
+    }
+}
---
+++ Appmon.Dashboard/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Appmon.Dashboard.Models;
+using Newtonsoft.Json;
+
+namespace Appmon.Dashboard.Controllers
+{
+    public class HealthCheckController : ApiController
+    {
+        public HealthCheckResults Get()
+        {
+            return (HealthCheckResults)JsonConvert.DeserializeObject(DocumentService.GetResult("DependencyHealthResultsCollection"), typeof(HealthCheckResults));
+        }
+    }
+}
