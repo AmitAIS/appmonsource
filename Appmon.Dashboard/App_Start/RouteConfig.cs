62deebfb46a51fa21e53ea305c70c358113efd01
skuchi <sk.kuchi.hcu@gmail.com>
2014-12-31 14:28:35 +0530

    commented default mvc route

--- Appmon.Dashboard/App_Start/RouteConfig.cs
+++ Appmon.Dashboard/App_Start/RouteConfig.cs
@@ -12,12 +12,14 @@ namespace BuildManager
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.IgnoreRoute("index.html"); //ignore the specific HTML start page
+            //routes.IgnoreRoute("");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
